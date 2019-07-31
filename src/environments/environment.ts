e36cf15fd751dd39260abea09a8518e7ac65069c
alerodriguezor <arodriguezor@outlook.com>
2019-07-31 08:40:04 -0300

    Initial j
    Initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCJd2ZNqyXkXm9kbvQmej7qkhhNd_BWcSI',
+    authDomain: 'tracker-taxis-29028.firebaseapp.com',
+    databaseURL: 'https://tracker-taxis-29028.firebaseio.com',
+    projectId: 'tracker-taxis-29028',
+    storageBucket: 'tracker-taxis-29028.appspot.com',
+    messagingSenderId: '304072576126'
+  }
+};
